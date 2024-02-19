6048b42dd99eff79e379f79c9736b5787adeac7e
Precipitator80 <rainlandsociety@gmail.com>
2024-02-19 14:24:26 +0000

    Added stacschecks for all outputs

---
+++ tests/output-wrf-l1l2/progRun.sh
@@ -0,0 +1,1 @@
+java -cp ".:../lib/fastjson2-2.0.45.android4.jar" CacheSimulator /cs/studres/CS4202/Coursework/P1-CacheSim/sample-inputs/l1l2.json /cs/studres/CS4202/Coursework/P1-CacheSim/trace-files/wrf.out
---
+++ tests/output-xz-_2way_lru/progRun.sh
@@ -0,0 +1,1 @@
+java -cp ".:../lib/fastjson2-2.0.45.android4.jar" CacheSimulator /cs/studres/CS4202/Coursework/P1-CacheSim/sample-inputs/_2way_lru.json /cs/studres/CS4202/Coursework/P1-CacheSim/trace-files/xz.out
